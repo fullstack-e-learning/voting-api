595c7823ade9f406911e495e7ef1443d2f0046c9
samitkumarpatel <samitkumarpatel@gmail.com>
2024-06-28 23:43:10 +0200

    addition

--- src/main/java/net/samitkumar/voting_api/VotingApiApplication.java
+++ src/main/java/net/samitkumar/voting_api/VotingApiApplication.java
@@ -1,10 +1,16 @@
 package net.samitkumar.voting_api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.ResponseCookie;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -21,8 +27,12 @@ import java.util.UUID;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class VotingApiApplication {
-	final ReactiveRedisTemplate<String, Vote> redisTemplate;
+	final ReactiveRedisTemplate<String, Vote> reactiveRedisTemplate;
+
 	static final String VOTE_ID_COOKIE = "vote_id";
+
+	@Value("${spring.data.redis.channel-name}")
+	private String channelName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VotingApiApplication.class, args);
@@ -40,21 +50,49 @@ public class VotingApiApplication {
 
 	private Mono<ServerResponse> vote(ServerRequest request) {
 		//read vote_id cookie from the request, If exists - it will be a 400 back , or else create a new one then send it back along with the response
-		if (Objects.isNull(request.cookies().getFirst(VOTE_ID_COOKIE))) {
-			return request
-					.bodyToMono(Vote.class)
-					.map(vote -> new Vote(UUID.randomUUID().toString(), vote.optionId()))
-					.map(vote -> redisTemplate.convertAndSend("votes", vote))
-					.flatMap(vote -> ServerResponse.ok()
-							.cookie(ResponseCookie.from(VOTE_ID_COOKIE, "").build())
-							.bodyValue(Map.of("message","SUCCESS"))
-					);
-		} else {
-			return ServerResponse.badRequest().build();
-		}
+		return Mono.justOrEmpty(request.cookies().getFirst(VOTE_ID_COOKIE))
+				.flatMap(cookie -> ServerResponse.badRequest().build())
+				.switchIfEmpty(
+						request.bodyToMono(Vote.class)
+								.map(vote -> new Vote(UUID.randomUUID().toString(), vote.optionId())) // Create a new Vote
+								.flatMap(vote ->
+										reactiveRedisTemplate.convertAndSend(channelName, vote)
+										.flatMap(redisReplyId ->
+												ServerResponse
+														.ok()
+														.cookie(ResponseCookie.from(VOTE_ID_COOKIE, vote.id()).build())
+														.bodyValue(Map.of("message","SUCCESS"))
+										)
+								)
+				);
 	}
-
-
 }
 
 record Vote(String id, String optionId) {}
+
+@Configuration
+class RedisConfig {
+
+	@Bean
+	public ReactiveRedisTemplate<String, Vote> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+
+		Jackson2JsonRedisSerializer<Vote> serializer = new Jackson2JsonRedisSerializer<>(Vote.class);
+		RedisSerializationContext.RedisSerializationContextBuilder<String, Vote> builder =
+				RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+		RedisSerializationContext<String, Vote> context = builder.value(serializer).build();
+		return new ReactiveRedisTemplate<>(factory, context);
+
+		/*
+		//OR
+		RedisSerializationContext<String, Vote> serializationContext = RedisSerializationContext
+				.<String, Vote>newSerializationContext(new StringRedisSerializer())
+				.key(new StringRedisSerializer())
+				.value(new Jackson2JsonRedisSerializer<>(Vote.class))
+				.hashKey(new Jackson2JsonRedisSerializer<>(String.class))
+				.hashValue(new Jackson2JsonRedisSerializer<>(Vote.class))
+				.build();
+
+		return new ReactiveRedisTemplate<>(factory, serializationContext);*/
+
+	}
+}
